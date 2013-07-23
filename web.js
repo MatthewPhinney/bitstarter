76670923f8b4a79a39c3c2947e04b248fcf18bdc
MatthewPhinney <matthew.d.phinney@gmail.com>
2013-07-23 19:41:54 -0400

    Updating web.js

--- web.js
+++ web.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
+var content;
+fs.readFileSync('./index.html', function read(err, data) {
+    if (err) {
+	throw err;
+    }
+    content = data;
+
+    console.log(content);
+    processFile();
+});
+
+function processFile() {
+    console.log(content);
+}
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
