95ba99d3c2ebb36e2dc72816db72d35b6cac8907
MatthewPhinney <matthew.d.phinney@gmail.com>
2013-07-23 19:58:32 -0400

    Updating web.js

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var express = require('express');
-var app = express();
 var fs = require('fs');
+var app = express();
 app.use(express.logger());
 
 var content = fs.readFileSync('./index.html');
 var buffer = new Buffer(content, 'utf8');
+
 app.get('/', function(request, response) {
   response.send(buffer.toString());
 });
