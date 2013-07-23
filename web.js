b6d332c53dcfbffe869177de3322a563fde3a53f
MatthewPhinney <matthew.d.phinney@gmail.com>
2013-07-23 19:46:01 -0400

    Revert "Updating web.js"
    This reverts commit 76670923f8b4a79a39c3c2947e04b248fcf18bdc.

--- web.js
+++ web.js
@@ -1,23 +1,9 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
-var content;
-fs.readFileSync('./index.html', function read(err, data) {
-    if (err) {
-	throw err;
-    }
-    content = data;
-
-    console.log(content);
-    processFile();
-});
-
-function processFile() {
-    console.log(content);
-}
 
 app.get('/', function(request, response) {
-  response.send(content);
+  response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
